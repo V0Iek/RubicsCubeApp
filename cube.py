83b7f832b39a57dd64ffdd17dfde7a749727e3f0
VOIek <michal.bernas2005@gmail.com>
2023-11-26 22:38:45 +0100

    Fixed all move methods. Now every move is done properly

--- cube.py
+++ cube.py
@@ -77,7 +77,7 @@ class RubicsCube:
     def vertical_turn(self, row, direction):
         if (row >= 0 and row <= 2) and (direction == 0 or direction == 1):
             if direction == 0: # Up
-                self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][row], self.state[3][1][row], self.state[3][2][row] = self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][2][row], self.state[3][1][row], self.state[3][0][row], self.state[0][2][row], self.state[0][1][row], self.state[0][0][row]
+                self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][2 - row], self.state[3][1][2 - row], self.state[3][2][2 - row] = self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][2][2 - row], self.state[3][1][2 - row], self.state[3][0][2 - row], self.state[0][2][row], self.state[0][1][row], self.state[0][0][row]
                 if row == 0:
                     self.state[4][0][0], self.state[4][0][1], self.state[4][0][2], self.state[4][1][2], self.state[4][2][2], self.state[4][2][1], self.state[4][2][0], self.state[4][1][0] = self.state[4][0][2], self.state[4][1][2], self.state[4][2][2], self.state[4][2][1], self.state[4][2][0], self.state[4][1][0], self.state[4][0][0], self.state[4][0][1]
                 elif row == 2:
@@ -85,7 +85,7 @@ class RubicsCube:
                 
 
             elif direction == 1: # Down
-                self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][row], self.state[3][1][row], self.state[3][2][row] = self.state[3][2][row], self.state[3][1][row], self.state[3][0][row], self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][2][row], self.state[5][1][row], self.state[5][0][row]
+                self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][2 - row], self.state[3][1][2 - row], self.state[3][2][2 - row] = self.state[3][2][2 - row], self.state[3][1][2 - row], self.state[3][0][2 - row], self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][2][row], self.state[5][1][row], self.state[5][0][row]
                 if row == 0:
                     self.state[4][0][0], self.state[4][0][1], self.state[4][0][2], self.state[4][1][2], self.state[4][2][2], self.state[4][2][1], self.state[4][2][0], self.state[4][1][0] = self.state[4][2][0], self.state[4][1][0], self.state[4][0][0], self.state[4][0][1], self.state[4][0][2], self.state[4][1][2], self.state[4][2][2], self.state[4][2][1]
                 elif row == 2:
