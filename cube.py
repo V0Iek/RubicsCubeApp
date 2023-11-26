bc389d6daa1abc811d40817b6de4919656d6e4a6
VOIek <michal.bernas2005@gmail.com>
2023-11-26 15:44:39 +0100

    Moved move functions to class and now they are methods

--- cube.py
+++ cube.py
@@ -1,7 +1,6 @@
 from colorama import Fore, Style, init
 from random import randint
 
-from moves import horizontal_turn, vertical_turn, side_turn
 from translate import translate
 
 class RubicsCube:
@@ -57,17 +56,40 @@ class RubicsCube:
         self.print_side(5)
         print("\n")
 
+
+    def horizontal_turn(self, row, direction):
+        if (row >= 0 and row <= 2) and (direction == 0 or direction == 1):
+            if direction == 0: # Left
+                self.state[1][row], self.state[2][row], self.state[3][row], self.state[4][row] = self.state[2][row], self.state[3][row], self.state[4][row], self.state[1][row]
+            elif direction == 1: # Right
+                self.state[1][row], self.state[2][row], self.state[3][row], self.state[4][row] = self.state[4][row], self.state[1][row], self.state[2][row], self.state[3][row]
+
+    def vertical_turn(self, row, direction):
+        if (row >= 0 and row <= 2) and (direction == 0 or direction == 1):
+            if direction == 0: # Up
+                self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][row], self.state[3][1][row], self.state[3][2][row] = self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][row], self.state[3][1][row], self.state[3][2][row], self.state[0][0][row], self.state[0][1][row], self.state[0][2][row]
+            elif direction == 1: # Down
+                self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row], self.state[3][0][row], self.state[3][1][row], self.state[3][2][row] = self.state[3][0][row], self.state[3][1][row], self.state[3][2][row], self.state[0][0][row], self.state[0][1][row], self.state[0][2][row], self.state[1][0][row], self.state[1][1][row], self.state[1][2][row], self.state[5][0][row], self.state[5][1][row], self.state[5][2][row]
+
+    def side_turn(self, row, direction):
+        if (row >= 0 and row <= 2) and (direction == 0 or direction == 1):
+            if direction == 0: # Clockwise
+                self.state[0][2 - row][0], self.state[0][2 - row][1], self.state[0][2 - row][2], self.state[2][0][row], self.state[2][1][row], self.state[2][2][row], self.state[5][row][0], self.state[5][row][1], self.state[5][row][2], self.state[4][0][2 - row], self.state[4][1][2 - row], self.state[4][2][2 - row] = self.state[4][0][2 - row], self.state[4][1][2 - row], self.state[4][2][2 - row], self.state[0][2 - row][0], self.state[0][2 - row][1], self.state[0][2 - row][2], self.state[2][0][row], self.state[2][1][row], self.state[2][2][row], self.state[5][row][0], self.state[5][row][1], self.state[5][row][2]
+            elif direction == 1: #Counter clockwise
+                self.state[0][2 - row][0], self.state[0][2 - row][1], self.state[0][2 - row][2], self.state[2][0][row], self.state[2][1][row], self.state[2][2][row], self.state[5][row][0], self.state[5][row][1], self.state[5][row][2], self.state[4][0][2 - row], self.state[4][1][2 - row], self.state[4][2][2 - row] = self.state[2][0][row], self.state[2][1][row], self.state[2][2][row], self.state[5][row][0], self.state[5][row][1], self.state[5][row][2], self.state[4][0][2 - row], self.state[4][1][2 - row], self.state[4][2][2 - row], self.state[0][2 - row][0], self.state[0][2 - row][1], self.state[0][2 - row][2]
+
+
     def scramble(self):
         for i in range(0, 20):
             x = randint(0, 2)
             y = randint(0, 2)
             z = randint(0, 1)
             if x == 0:
-                horizontal_turn(self.state, y, z)
+                self.horizontal_turn(y, z)
                 print(translate("horizontal", y, z))
             elif x == 1:
-                vertical_turn(self.state, y, z)
+                self.vertical_turn(y, z)
                 print(translate("vertical", y, z))
             elif x == 2:
-                side_turn(self.state, y, z)
+                self.side_turn(y, z)
                 print(translate("side", y, z))
