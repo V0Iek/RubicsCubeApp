83c5d3f11d590011c92f9e80ed9d0dff9734e41b
VOIek <michal.bernas2005@gmail.com>
2023-11-26 20:30:08 +0100

    Now you can do something in playground

--- playground.py
+++ playground.py
@@ -15,7 +15,20 @@ def playground(cube):
         cube.print_cube()
         print("Aby wyjść wybierz 1")
         choose = input()
-        if choose == "L":
+        if choose == "U":
+            cube.horizontal_turn(0, 0)
+        elif choose == "U'":
+            cube.horizontal_turn(0, 1)
+        elif choose == "E":
+            cube.horizontal_turn(1, 1)
+        elif choose == "E'":
+            cube.horizontal_turn(1, 0)
+        elif choose == "D":
+            cube.horizontal_turn(2, 1)
+        elif choose == "D'":
+            cube.horizontal_turn(2, 0)
+
+        elif choose == "L":
             cube.vertical_turn(0, 1)
         elif choose == "L'":
             cube.vertical_turn(0, 0)
@@ -27,3 +40,16 @@ def playground(cube):
             cube.vertical_turn(2, 0)
         elif choose == "R'":
             cube.vertical_turn(2, 1)
+
+        elif choose == "F":
+            cube.side_turn(0, 0)
+        elif choose == "F'":
+            cube.side_turn(0, 1)
+        elif choose == "S":
+            cube.side_turn(1, 0)
+        elif choose == "S'":
+            cube.side_turn(1, 1)
+        elif choose == "B":
+            cube.side_turn(2, 1)
+        elif choose == "B'":
+            cube.side_turn(2, 0)
