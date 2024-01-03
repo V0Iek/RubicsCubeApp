7bdeabcd39f21aac3da802b756ffb64e0ed9dd0a
VOIek <michal.bernas2005@gmail.com>
2024-01-03 20:16:23 +0100

    Switching to web app

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="description" content=""/>
+		<meta name="keyworlds" content=""/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+		<title> Rubics Cube </title>
+
+		<!--[if lt IE 9]>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<![endif]-->
+		
+		<link rel="stylesheet" href="style.css"/>
+	</head>
+	
+	<body>
+
+	</body>
+</html>
